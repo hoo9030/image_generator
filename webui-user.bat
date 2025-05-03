d2f0a209eee034968da8118ab074bc221b898f0b
hoo9030 <hoo9030@gmail.com>
2025-05-04 00:51:14 +0900

    Reconnect to GitHub

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--skip-torch-cuda-test --opt-sdp-attention --medvram --no-half-vae 
+set COMMANDLINE_ARGS=--skip-torch-cuda-test --opt-sdp-attention --medvram --no-half-vae --api
 
 call webui.bat
